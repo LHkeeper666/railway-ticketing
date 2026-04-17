94f3a88e8f8afa5ae93441bb03a17f65fe2c8023
LHkeeper666 <l1367858987@outlook.com>
2026-04-17 16:54:00 +0800

    init: 创建项目

---
+++ src/test/java/com/lhkeeper/ticketing/railway_ticketing/RailwayTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhkeeper.ticketing.railway_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
